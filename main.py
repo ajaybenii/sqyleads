a5963619f49a9a5ff03c14535a13797a5c879916
Ajay Beniwal <85953534+ajaybenii@users.noreply.github.com>
2021-11-19 10:36:30 +0530

    update jason and remove class
    use api for postman

--- main.py
+++ main.py
@@ -1,50 +1,39 @@
-from fastapi import FastAPI,File, Form, UploadFile
+from fastapi import FastAPI,File, Form, UploadFile, Response
+from fastapi import Request, FastAPI
 from pydantic import BaseModel
 import pandas as pd
 from io import StringIO
 import pickle
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
+import json
 
 
 app=FastAPI()
 
-
-class IrisSpecies(BaseModel):
-    Searches: float
-    Filters: float
-    Shortlist: float
-    Enquiries: float
-    Photoclicks: float
-    PageView: float
-        
-                                        
+df1 = pd.read_csv('dfl.csv')
+df1 = df1[["Searches","Filters","Shortlist","Enquiries","Photoclicks","PageView"]]
+                                               
 @app.get('/')
 def index():
     
     return {'message': "This is the home page of this API. Go to /apiv1/ or /apiv2/?name="}
-df1 = pd.read_csv('dfl.csv')
-df1 = df1[["Searches","Filters","Shortlist","Enquiries","Photoclicks","PageView"]]
 
 
 @app.post('/predict')
-async def predict_species(result: IrisSpecies):
-    data12 = result.dict()
-    data=pd.DataFrame([data12])
-    print(data)
-    
-    data_1=(data-df1.min())/(df1.max()-df1.min())
-    print(data_1)
-    
-    
-    loaded_model = pickle.load(open('fmodel.pkl', 'rb'))
-    #data_in = [[data_1['Searches'], data_1['Filters'], data_1['Shortlist'], data_1['Enquiries'], data_1['Photoclicks'], data_1['PageView']]]
+async def predict_species(request:Request):
+    data12 = await request.json()
+    test = pd.DataFrame(data12)
+    data_1=(test-df1.min())/(df1.max()-df1.min())#Normaalized the input data
+
+    loaded_model = pickle.load(open('fmodel.pkl', 'rb'))#Load the model
     prediction = loaded_model.predict(data_1)
-    #probability = loaded_model.predict_proba(data_in).max()
+    data_1["Result"]=prediction#store final value in result
+
+    data1=[['Searches','Filters','Shortlist','Enquiries','Photoclicks','PageView','Result']]
+    orient="records"
+    result=data_1.to_json(orient=orient)
     
-    return {
-    'prediction': prediction[0],
-    # 'probability': probability
-    }
+    return Response(result)
 
 
