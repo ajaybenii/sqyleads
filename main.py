a361a44a7576a32b8a341557efdebe27cb7d7c28
Ajay Beniwal <85953534+ajaybenii@users.noreply.github.com>
2021-12-14 16:36:31 +0530

    update new model LR
    comment old model fmodel.pkl

--- main.py
+++ main.py
@@ -18,22 +18,48 @@ df1 = df1[["Searches","Filters","Shortlist","Enquiries","Photoclicks","PageView"
 def index():
     
     return {'message': "This is the home page of this API. Go to /apiv1/ or /apiv2/?name="}
-
-
+  
+  
+  
 @app.post('/predict')
 async def predict_species(request:Request):
     data12 = await request.json()
-    test = pd.DataFrame(data12)
+    initial_data = pd.DataFrame(data12)
+    print(initial_data.head())
+
+    test=initial_data[['Searches','Filters','Shortlist','Enquiries','Photoclicks','PageView']]
     data_1=(test-df1.min())/(df1.max()-df1.min())#Normaalized the input data
+    print("data",data_1.head())
 
-    loaded_model = pickle.load(open('fmodel.pkl', 'rb'))#Load the model
+    loaded_model = pickle.load(open('model_L7d.pkl', 'rb'))#Load the model
+    # prediction = loaded_model.predict(data_1)
     prediction = loaded_model.predict(data_1)
-    data_1["Result"]=prediction#store final value in result
+    prediction2 = loaded_model.predict_proba(data_1)
 
-    data1=[['Searches','Filters','Shortlist','Enquiries','Photoclicks','PageView','Result']]
+    initial_data["Result"] = prediction
+    print(initial_data.head())
+    initial_data[["Prob_0","Prob_1"]] = prediction2
+    print(initial_data.head())
+ 
     orient="records"
-    result=data_1.to_json(orient=orient)
+    final_result=initial_data.to_json(orient=orient)
+    return Response(final_result)
+
+# Old code
+# @app.post('/predict')
+# async def predict_species(request:Request):
+#     data12 = await request.json()
+#     test = pd.DataFrame(data12)
+#     data_1=(test-df1.min())/(df1.max()-df1.min())#Normaalized the input data
+
+#     loaded_model = pickle.load(open('fmodel.pkl', 'rb'))#Load the model
+#     prediction = loaded_model.predict(data_1)
+#     data_1["Result"]=prediction#store final value in result
+
+#     data1=[['Searches','Filters','Shortlist','Enquiries','Photoclicks','PageView','Result']]
+#     orient="records"
+#     result=data_1.to_json(orient=orient)
     
-    return Response(result)
+#     return Response(result)
 
 
